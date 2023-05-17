d55afddefb6e3358bbd8cc2e9b6a6344cd9e5e08
gowthamket <65178336+gowthamket@users.noreply.github.com>
2023-05-16 22:33:03 -0400

    Camera Controller

---
+++ Combat System/Assets/Scripts/Comp_PlayerInputs.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gowtham
+{
+    public class Comp_PlayerInputs : MonoBehaviour
+    {
+        public const string MouseXString = "Mouse X";
+        public const string MouseYString = "Mouse Y";
+        public const string MouseScrollString = "MouseScrollWheel";
+
+        public static float MouseXInput { get => UnityEngine.Input.GetAxis(MouseXString); }
+        public static float MouseYInput { get => UnityEngine.Input.GetAxis(MouseYString); }
+        public static float MouseScrollInput { get => UnityEngine.Input.GetAxis(MouseScrollString); }
+    }
+}
+
